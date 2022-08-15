e7a0b961ef59fe5c73b4db75b6be97c8649535c8
Saheed Simisaye <st.simisaye@gmail.com>
2022-08-15 08:47:41 +0100

    updated directory component

---
+++ src/components/category-item/CategoryItem.js
@@ -0,0 +1,19 @@
+import "../category-item/categories.styles.scss";
+
+const CategoryItem = ({ category }) => {
+  const { title, imageUrl } = category;
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+      <div className="category-body-container">
+        <h2>{title.toLocaleUpperCase()}</h2>
+        <p>SHOP NOW</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
---
+++ src/components/directory/Directory.js
@@ -0,0 +1,15 @@
+import './Directory.styles.scss'
+import CategoryItem from '../category-item/CategoryItem';
+
+const Directory = ({categories}) => {
+
+    return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+}
+
+export default Directory;
